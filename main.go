f30d082ba509d9c0f06b4ccdcd08641eb0c09fc6
LordMoMA <lihongyi0512@gmail.com>
2023-11-07 16:34:02 +0800

    “flag-line”

--- main.go
+++ main.go
@@ -1,33 +1,38 @@
 package main
 
 import (
-  "bufio"
-  "fmt"
-  "io"
-  "os"
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-  // Calling the count function to count the number of words
-  // received from the Standard Input and printing it out
-  fmt.Println(count(os.Stdin))
+	lines := flag.Bool("l", false, "count lines")
+	flag.Parse()
+	// Calling the count function to count the number of words
+	// received from the Standard Input and printing it out
+	fmt.Println(count(os.Stdin, *lines))
 }
 
-func count(r io.Reader) int {
-  // A scanner is used to read text from a Reader (such as files)
-  scanner := bufio.NewScanner(r)
+func count(r io.Reader, countLines bool) int {
+	// A scanner is used to read text from a Reader (such as files)
+	scanner := bufio.NewScanner(r)
 
-  // Define the scanner split type to words (default is split by lines)
-  scanner.Split(bufio.ScanWords)
+	// Define the scanner split type to words (default is split by lines)
+	if !countLines {
+		scanner.Split(bufio.ScanWords)
+	}
 
-  // Defining a counter
-  wc := 0
+	// Defining a counter
+	wc := 0
 
-  // For every word scanned, increment the counter
-  for scanner.Scan() {
-    wc++
-  }
+	// For every word scanned, increment the counter
+	for scanner.Scan() {
+		wc++
+	}
 
-  // Return the total
-  return wc
+	// Return the total
+	return wc
 }
